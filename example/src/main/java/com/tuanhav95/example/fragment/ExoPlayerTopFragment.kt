e655be73113e1e543f1217897755398e1f18bc99
tuanhacitigo <tuan.ha@citigo.com.vn>
2020-06-24 08:51:38 +0700

    add example exoplayer

--- example/src/main/java/com/tuanhav95/example/fragment/ExoPlayerTopFragment.kt
+++ example/src/main/java/com/tuanhav95/example/fragment/ExoPlayerTopFragment.kt
@@ -8,15 +8,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import com.tuanhav95.example.R
 import kotlinx.android.synthetic.main.fragment_top_exo_player.*
 
-class ExoPlayerTopFragment : Fragment() {
+class ExoPlayerTopFragment : Fragment(), Player.EventListener {
 
     companion object {
         const val STREAM_URL = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
@@ -38,17 +40,28 @@ class ExoPlayerTopFragment : Fragment() {
         mediaDataSourceFactory = DefaultDataSourceFactory(context, Util.getUserAgent(context, "mediaPlayerSample"))
 
         val mediaSource = ProgressiveMediaSource.Factory(mediaDataSourceFactory).createMediaSource(Uri.parse(STREAM_URL))
-
+        simpleExoPlayer.addListener(this)
         simpleExoPlayer.prepare(mediaSource, false, false)
         simpleExoPlayer.playWhenReady = true
 
         playerView.setShutterBackgroundColor(Color.TRANSPARENT)
         playerView.player = simpleExoPlayer
         playerView.requestFocus()
+
+        aspectRatioFrameLayout.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIXED_WIDTH
+        aspectRatioFrameLayout.setAspectRatio(16f / 9)
+    }
+
+    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+        super.onPlayerStateChanged(playWhenReady, playbackState)
+        if (playbackState == Player.STATE_ENDED) {
+            simpleExoPlayer.playWhenReady = true
+        }
     }
 
     override fun onStop() {
         super.onStop()
+        simpleExoPlayer.removeListener(this)
         simpleExoPlayer.release()
     }
 
